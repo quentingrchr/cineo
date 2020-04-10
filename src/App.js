730b5bc0a7ea83e9f15108a3c1e7825cd134784c
bastien17-dev <bastien.paquier@gmail.com>
2020-04-10 15:37:03 +0200

    edit html of header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
-import Button from "./components/button/button.component";
-import "./styles.scss";
+import React from 'react';
+import Header from './components/header/header.component';
+import Button from './components/button/button.component';
+import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Header />
       <h1>Work In Progress</h1>
       <Button />
     </div>
