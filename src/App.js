6141b5b7aba91ea41ece732e27d78f5da0c577ce
bastien17-dev <bastien.paquier@gmail.com>
2020-04-11 19:02:23 +0200

    login page reoganization first part

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import SignInPage from "./pages/sign-in/sign-in.page";
-import SignUpPage from "./pages/sign-up/sign-up.page";
-import MoviesPage from "./pages/movies/movies.page";
-import SeriesPage from "./pages/series/series.page";
-import MyListPage from "./pages/my-list/my-list.page";
-import HomePage from "./pages/home/home.page";
-import DevQuentin from "./pages/dev-quentin/dev-quentin";
+import SignInUpPage from './pages/sign-in-up/sign-in-up.page';
+import MoviesPage from './pages/movies/movies.page';
+import SeriesPage from './pages/series/series.page';
+import MyListPage from './pages/my-list/my-list.page';
+import HomePage from './pages/home/home.page';
+import DevQuentin from './pages/dev-quentin/dev-quentin';
 
-import "./styles.scss";
-
+import './styles.scss';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/dev-quentin" component={DevQuentin} />
-        <Route exact path="/home" component={HomePage} />
-        <Route exact path="/sign-in" component={SignInPage} />
-        <Route exact path="/sign-up" component={SignUpPage} />
-        <Route exact path="/movies" component={MoviesPage} />
-        <Route exact path="/series" component={SeriesPage} />
-        <Route exact path="/my-list" component={MyListPage} />
+        <Route exact path='/dev-quentin' component={DevQuentin} />
+        <Route exact path='/home' component={HomePage} />
+        <Route exact path='/sign-in-up' component={SignInUpPage} />
+        <Route exact path='/movies' component={MoviesPage} />
+        <Route exact path='/series' component={SeriesPage} />
+        <Route exact path='/my-list' component={MyListPage} />
       </Switch>
     </Router>
   );
-
 }
 
 export default App;
---
+++ src/pages/sign-in-up/sign-in-up.page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignInUp from '../../components/sign-in-up/SignInUp.component';
+
+function SignInUpPage() {
+  return (
+    <div>
+      <SignInUp />
+    </div>
+  );
+}
+
+export default SignInUpPage;
