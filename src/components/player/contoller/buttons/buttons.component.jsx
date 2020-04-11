b27f38bbd0574838ac58c858030efb414128f6de
bastien17-dev <bastien.paquier@gmail.com>
2020-04-11 13:01:30 +0200

    volume done

--- src/components/player/contoller/buttons/buttons.component.jsx
+++ src/components/player/contoller/buttons/buttons.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Play from './play-pause/play.component';
+import Volume from './volume/volume.component';
+import Scroll from './scroll/scroll.component';
 
 export default () => (
   <div className='controler__buttons'>
-    <Play />
+    <div className='controler__play-volume'>
+      <Play />
+      <Scroll />
+      <Volume />
+    </div>
   </div>
 );
