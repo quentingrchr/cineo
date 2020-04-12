b00d5f1dde70a39588f9a2e84ce1294d91f204cd
Cours <Cours@MacBook-Pro-de-Thomas-2.local>
2020-04-12 16:12:03 +0200

    integration series page

--- src/pages/movies/movies.page.jsx
+++ src/pages/movies/movies.page.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "../../components/header/header.component";
+import Footer from "../../components/footer/footer.component";
+import Slider from "../../components/slider/slider.component";
+import data from "../../data.json";
 
 export default function moviesPage() {
   return (
     <div>
       <Header />
-      <h1>Welcome to our "Movies Page"</h1>
+      <h1>Films</h1>
+      <div>
+        <p></p>
+        <Slider
+        data={data.filter((movie) => {
+          return movie.type === "serie";
+        })}
+      />
+      </div>
+      <Footer />
     </div>
   );
 }
