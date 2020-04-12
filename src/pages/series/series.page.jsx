4dfb3371d3847866be95ea3b6e5a7b8171aec0a1
Cours <Cours@MacBook-Pro-de-Thomas-2.local>
2020-04-12 16:51:25 +0200

    film page ok and fix other details

--- src/pages/series/series.page.jsx
+++ src/pages/series/series.page.jsx
@@ -2,62 +2,61 @@ import React from "react";
 import Header from "../../components/header/header.component";
 import Footer from "../../components/footer/footer.component";
 import Slider from "../../components/slider/slider.component";
+import Title from '../../components/title/title.component';
 import data from "../../data.json";
+import Top5 from '../../components/top5/top5Section.component';
 
 export default function seriesPage() {
   return (
-    <div className='series__container'>
+    <div className='series__page'>
       <Header />
-      <h1 className='series__title'>Séries</h1>
-      <div className='series_component'>
-        <p>Les plus gros succès Cinéo</p>
-        <Slider
-        data={data.filter((movie) => {
-          return movie.type === "serie";
-        })}
-      />
+      <div className='series__container'>
+        <h1 className='series__title'>Séries</h1>
+        <div className='series__section'>
+          <Title content='Les plus gros succès Cinéo'/>
+          <Slider
+          data={data.filter((movie) => {
+            return movie.type === "serie";
+          })}
+        />
+        </div>
+        <div className='series__section large__slider'>
+          <Title content='Les séries originales Cinéo'/>
+          <Slider
+          data={data.filter((movie) => {
+            return movie.type === "serie";
+          })}
+        />
+        </div>
+        <div className='series__section'>
+          <Title content='Nouvelles séries'/>
+          <Slider
+          data={data.filter((movie) => {
+            return movie.type === "serie";
+          })}
+        />
+        </div>
+        <div className='series__section'>
+          <Title content='Animés'/>
+          <Slider
+          data={data.filter((movie) => {
+            return movie.type === "serie";
+          })}
+        />
+        </div>
+        <div className='series__section'>
+          <Title content='Documentaire' />
+          <Slider
+          data={data.filter((movie) => {
+            return movie.type === "serie";
+          })}
+        />
+        </div>
+        <div className='series__section large__slider'>
+          <Title content='Top 5 de la semaine'/>
+          <Top5 />
+        </div>
       </div>
-      <div className='series_component'>
-        <p>Les séries originales Cinéo</p>
-        <Slider
-        data={data.filter((movie) => {
-          return movie.type === "serie";
-        })}
-      />
-      </div>
-      <div className='series_component'>
-        <p>Nouvelles séries</p>
-        <Slider
-        data={data.filter((movie) => {
-          return movie.type === "serie";
-        })}
-      />
-      </div>
-      <div className='series_component'>
-        <p>Animés</p>
-        <Slider
-        data={data.filter((movie) => {
-          return movie.type === "serie";
-        })}
-      />
-      </div>
-      <div className='series_component'>
-        <p>Documentaire</p>
-        <Slider
-        data={data.filter((movie) => {
-          return movie.type === "serie";
-        })}
-      />
-      </div>
-      <div className='series_component'>
-        <p>Top 5 de la semaine</p>
-        <Slider
-        data={data.filter((movie) => {
-          return movie.type === "serie";
-        })}
-      />
-      </div>
-
       <Footer />
     </div>
   );
