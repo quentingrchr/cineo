29c7b60379166e35434c623320e7a0a4607f3a91
Cours <Cours@MacBook-Pro-de-Thomas-2.local>
2020-04-14 10:38:48 +0200

    fixing margins and addind see more cta :white_check_mark:

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -30,7 +30,7 @@ export default class GomePage extends Component {
   componentWillMount() {
     setTimeout(() => {
       this.setState({ videoIsVisible: true });
-    }, 8000);
+    }, 3000);
   }
 
   render() {
@@ -58,7 +58,7 @@ export default class GomePage extends Component {
             alt="background"
           />
 
-          <div className="first-category">
+          <div className="first-category category">
             <Title content="Les nouvelles séries" />
             <Slider data={seriesOnly(data, 15).reverse()} />
           </div>
@@ -84,6 +84,12 @@ export default class GomePage extends Component {
           </div>
           <div className="category">
             <Title content="Ils arrivent bientôt..." />
+            <div className='cta__see__more'>
+              <div>Voir l'agenda des sorties</div>
+              <svg viewBox="0 0 21 16">
+                <path d="M0.292893 7.29289C-0.097631 7.68342 -0.0976311 8.31658 0.292893 8.7071L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34314C8.46159 1.95262 8.46159 1.31946 8.07107 0.928931C7.68054 0.538407 7.04738 0.538407 6.65685 0.928931L0.292893 7.29289ZM21 7L1 7L1 9L21 9L21 7Z" />
+              </svg>
+            </div>
             <ComingSoon data={comingSoon(data)} />
           </div>
         </section>
